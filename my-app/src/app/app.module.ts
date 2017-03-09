30402e4451b0b15d1a15845ed91b784d8e2da109
Oterem <ot1234ster@gmail.com>
2017-03-09 15:38:37 +0200

    tirgul

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { LitemComponent } from './litem/litem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    LitemComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-app/src/app/litem/litem.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-litem',
+  templateUrl: './litem.component.html',
+  styleUrls: ['./litem.component.css']
+})
+export class LitemComponent implements OnInit {
+
+  @Input() item;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
